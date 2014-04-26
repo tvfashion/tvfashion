24182e9975d629be275cc1b1bcf6102edf1f2664
akthechamp <akthechamp@gmail.com>
2014-04-27 01:55:31 +0530

    Added Support for TvDb

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,5 @@
 class Show < ActiveRecord::Base
 	attr_accessible :name, :overview, :first_aired, :genres, :network, :rating, :runtime, :air_time, :imdb_id, :episodes_count, :actors_count, :seasons_count, :status, :airs_dayofweek, :rating_count
+
+	has_many :banner
 end
